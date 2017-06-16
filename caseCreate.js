fa67dd7122886bb0e20cb9b63629361b04d65466
Cristian Alonso Aldama <caldama@salesforce.com>
2017-06-16 12:48:03 +0200

    Fixed postgres errors

--- caseCreate.js
+++ caseCreate.js
@@ -82,7 +82,7 @@ module.exports = {
 		    query.on('end', function(result) {
           		console.log("CASE Query ended");
           		// need to wait until heroku connect synchs and brings back sfid. Very bad programming technique, but hey, this is a demo
-          		sleep.sleep(5);
+          		sleep.sleep(10);
           		queryCount--;
            		if (result) {
                		//Obtain SalesforceID we need for live chat transcript
